9b209a38c6fe03ac2ee4ee409d40b310ff80067e
Constance Gobert <constancegobert@hotmail.fr>
2017-08-04 17:08:24 +0200

    creating messages lists

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,17 +1,23 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
          var $ctrl = this;
          $ctrl.title = "Bloc Chat";
          $ctrl.rooms = Room.all;
+         $ctrl.activeRoom = null;
          $ctrl.RoomModal = function() {
              $uibModal.open({
                  templateUrl: '/templates/modal.html',
                  controller: 'ModalCtrl as modals'
              });
          }
-    }
+
+         $ctrl.setActiveRoom = fonction(room) {
+           $ctrl.activeRoom = room;
+           $ctrl.messages = Message.getByRoomId($ctrl.activeRoom.$id);
+         }
+    };
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
